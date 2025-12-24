1c219d69e421d37f31629c21dffbdd53019ee8bf
Jageshwar <kumarjageshwar007t7@gmail.com>
2025-12-24 17:20:43 +0530

    Update FSM3_asynchronous_reset.v

--- FSM3_asynchronous_reset.v
+++ FSM3_asynchronous_reset.v
@@ -88,4 +88,65 @@ end
 endmodule
 
 // My corrected Code:-
+// Not getting correct output;
+// fix this :-
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); //
 
+    parameter a=0, b=1, c=2, d=3;
+    reg [1:0]next_state,state;
+    reg out1;
+
+    always @(posedge clk or posedge areset)begin 
+        if(areset==1'b1)begin 
+            state = a;
+        end
+        else begin 
+            state = next_state;
+        end
+    end
+
+    always @(*)begin 
+        case(state)
+            a: begin
+                next_state = (in)?b:a;
+            end
+            b: begin 
+                next_state = (in)?b:c;
+            end
+            c: begin 
+                next_state = (in)?d:a;
+            end
+            d: begin 
+                next_state = (in)?b:c;
+            end
+            default: begin 
+                next_state = a;
+            end
+        endcase
+    end
+
+    always @(*)begin 
+        case(state)
+            a:begin 
+                out1 = 1'b0;
+            end
+            b:begin 
+                out1 = 1'b0;
+            end
+            c:begin 
+                out1 = 1'b0;
+            end
+            d:begin 
+                out1 = 1'b1;
+            end
+            default: begin 
+                out1 = 1'b0;
+            end
+        endcase
+    end
+    assign out = out1;
+endmodule
