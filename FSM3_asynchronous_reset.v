61e39b7a20554be85c0dc3781ffdde4e2a71f737
Jageshwar <kumarjageshwar007t7@gmail.com>
2025-12-24 10:52:09 +0530

    Create FSM3_asynchronous_reset.v

---
+++ FSM3_asynchronous_reset.v
@@ -0,0 +1,46 @@
+// Not getting correct output;
+// fix this :-
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); //
+
+    parameter a=0, b=1, c=2, d=3;
+    reg [1:0]state;
+    reg out1;
+    reg next_state;
+    
+    always @(negedge clk or posedge areset) begin 
+        if(areset==1'b1)begin 
+            state = a;
+            out1 = 1'b0;
+            
+        end
+        else begin
+            case(state)
+                a:begin 
+                    state = (in)?b:a;
+                    out1 = 1'b0;
+                end
+                b:begin 
+                    state = (in)?b:c;
+                    out1 = 1'b0;
+                end
+                c:begin 
+                    state = (in)?d:a;
+                    out1 = 1'b0;
+                end
+                d:begin 
+                    state = (in)?b:c;
+                    out1 = 1'b1;
+                end
+				default: begin 
+                    state = (in)?b:a;
+                    out1 = 1'b0;
+                end
+            endcase
+        end
+    end
+    assign out = out1;
+endmodule
