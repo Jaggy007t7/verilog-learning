922ea29a2105b6d6d1f37e9919a874fe2e918916
Jageshwar <kumarjageshwar007t7@gmail.com>
2026-06-02 11:07:08 +0530

    Create synth_design.v

---
+++ Pipelining_Using_Verilog/example2/synth_design.v
@@ -0,0 +1,66 @@
+// Non synthesizable version:-
+`timescale 1ns/1ps
+
+module alu #(
+    parameter size=16  // Bits;
+)(
+    input wire clk1, clk2,       // -> To avoid race around condition.
+    input wire write
+    input wire [3:0]rs1,rs2,rd,
+    input [2:0]func,             // -> Op-code.
+    input wire [7:0]addr,       //  -> Addr. for main memory.
+    output wire [size-1:0]Zout
+);
+
+// Register bank;
+reg [size-1:0]register_bank[0:size-1];
+
+// Memory;
+reg [size-1:0]mem[0:255];
+
+// latches;
+reg [size-1:0]l1_a,l1_b,l1_rd,l1_func,l1_addr;
+reg [size-1:0]l2_z,l2_rd,l2_addr;
+reg [size-1:0]l3_z,l3_addr;
+reg [size-1:0]l4_z;
+
+always @(posedge clk1)begin 
+    l1_a<=register_bank[rs1];
+    l1_b<=register_bank[rs2];
+    l1_rd<=func;
+    l1_adder<=addr;
+end
+
+always @(negedge clk2)begin 
+    case(func)
+        1: l2_z=l1_a+l1_b;
+        2: l2_z=l1_a-l1_b;
+        3: l2_z=l1_a*l1_b;
+        4: l2_z=l1_a;
+        5: l2_z=l1_b;
+        6: l2_z=l1_a^l1_b;
+        7: l2_z=l1_a|l1_b;
+        8: l2_z=l1_a&l1_b;
+        default: func=1;
+
+    endcase
+    l2_rd <= l1_rd;
+    l2_addr <= l1_addr;
+
+end
+
+always@(posedge clk1)begin 
+    register_bank[l2_rd]<=l2_z;
+    l3_z<=l2_z;
+    l3_add<=l2_addr;
+
+
+end
+
+always @(negedge clk2)begin 
+    mem[l3_addr]<=l3_z;
+end
+
+assign Zout = l3_z;
+
+endmodule
